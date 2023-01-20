a0fbc58a7188cfd85afb00b810356aa3df0165af
Patrick Nicodemus <gadget142@gmail.com>
2023-01-20 15:53:18 -0500

    Created new stubs.

---
+++ stubs/ipdb/__init__.pyi
@@ -0,0 +1,2 @@
+from ipdb.__main__ import iex as iex, launch_ipdb_on_exception as launch_ipdb_on_exception, pm as pm, post_mortem as post_mortem, run as run, runcall as runcall, runeval as runeval, set_trace as set_trace
+from ipdb.stdout import slaunch_ipdb_on_exception as slaunch_ipdb_on_exception, spm as spm, spost_mortem as spost_mortem, sset_trace as sset_trace
