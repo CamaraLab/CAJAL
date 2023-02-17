2df094b2a23663a72a187abe5f5550a2358d3c2d
Patrick Nicodemus <gadget142@gmail.com>
2023-02-17 10:10:13 -0500

    Deleted a commented-out function from sample_seg.

--- src/cajal/sample_seg.py
+++ src/cajal/sample_seg.py
@@ -12,43 +12,6 @@ from pathos.pools import ProcessPool
 
 from cajal.utilities import pj
 
-# def save_cell_boundaries(imarray, outfile, n_sample, background=0):
-#     """
-#     Save n pixel coordinates sampled from the boundary of each cell in a segmented image,
-#     skipping cells that touch the border of the image
-
-#     Args:
-#         imarray (numpy array): 2D segmented image where the pixels belonging to \
-#         different cells have different values
-#         outfile (string): file path to save cell boundaries - output files will be named\
-#          with _cellN.csv where N is
-#             the pixel value denoting that cell in the segmented image
-#         n_sample (integer): number of pixel coordinates to sample from boundary of each cell
-#         background (integer, float): value of background pixels, this will not be saved as a boundary
-
-#     Result:
-#         None (writes to file)
-#     """
-#     cell_ids = set(np.unique(imarray))
-#     remove_cells = set()
-#     remove_cells.update(np.unique(imarray[0, :]))
-#     remove_cells.update(np.unique(imarray[-1, :]))
-#     remove_cells.update(np.unique(imarray[:, 0]))
-#     remove_cells.update(np.unique(imarray[:, -1]))
-#     cell_ids = list(cell_ids.difference(remove_cells))
-#     for cell in cell_ids:
-#         if cell == background:  # Don't draw a boundary around background
-#             continue
-#         cell_imarray = (imarray == cell) * 1
-#         boundary_pts = measure.find_contours(cell_imarray, 0.5, fully_connected='high')
-#         if len(boundary_pts) > 1:
-#             warnings.warn("More than one boundary for cell " + str(cell))
-#             continue
-#         if boundary_pts[0].shape[0] < n_sample:
-#             warnings.warn("Fewer than " + str(n_sample) + " pixels around boundary of cell " + str(cell))
-#         np.savetxt(outfile.replace(".csv","") + "_cell"+str(cell)+".csv",
-#                    boundary_pts[0][np.linspace(0, boundary_pts[0].shape[0]-1, n_sample).astype("uint32")],
-#                    delimiter=",")
 
 def cell_boundaries(imarray : npt.NDArray[np.int_],
                     n_sample: int,
