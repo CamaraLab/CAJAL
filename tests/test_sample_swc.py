b1fb93fafc1c84e05b082ebeffc4a52e5b74c076
Patrick Nicodemus <gadget142@gmail.com>
2023-03-30 16:02:48 -0400

    Added tests for sample_swc.

--- tests/test_sample_swc.py
+++ tests/test_sample_swc.py
@@ -1,7 +1,40 @@
-from src.cajal.sample_swc import compute_icdm_all_euclidean, compute_icdm_all_geodesic
+# from src.cajal.sample_swc import compute_icdm_all_euclidean, compute_icdm_all_geodesic
+from src.cajal.swc import get_filenames, default_name_validate
+from src.cajal.sample_swc import (
+    read_preprocess_compute_geodesic,
+    read_preprocess_compute_euclidean,
+    compute_icdm_all_euclidean,
+    compute_icdm_all_geodesic,
+)
+from src.cajal.utilities import Err
 
 
-def test_compute_geodesic():
+def test_rpcg():
+    swc_dir = "CAJAL/data/swc"
+    cell_names, file_paths = get_filenames(swc_dir, default_name_validate)
+    t = map(
+        lambda file_path: read_preprocess_compute_geodesic(
+            file_path, 20, lambda forest: forest[0]
+        ),
+        file_paths,
+    )
+    for i, a in enumerate(t):
+        if i > 20:
+            break
+    t = map(
+        lambda file_path: read_preprocess_compute_euclidean(
+            file_path,
+            20,
+            lambda forest: forest if len(forest) == 1 else Err("Too many components."),
+        ),
+        file_paths,
+    )
+    for i, a in enumerate(t):
+        if i > 20:
+            break
+
+
+def test_compute_icdm_both():
     swc_dir = "CAJAL/data/swc"
     compute_icdm_all_euclidean(
         infolder=swc_dir,
