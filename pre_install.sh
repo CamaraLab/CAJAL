d1169f5745ec34ddbdf3e9d26da094b72e28d15d
Patrick Nicodemus <gadget142@gmail.com>
2023-07-12 14:15:56 -0400

    Still trying to update Pandoc.

--- pre_install.sh
+++ pre_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-linux-amd64.tar.gz
-tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C /usr/local
-export PATH="/usr/local:$PATH"
+tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C ~/.local
+export PATH="~/.local:$PATH"
 echo $(whereis pandoc)
 echo $(which pandoc)
