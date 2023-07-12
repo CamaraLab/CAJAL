42d58eea3f50d1bbe74603ec201d7a58808b227b
Patrick Nicodemus <gadget142@gmail.com>
2023-07-12 13:43:17 -0400

    updated preinstall script.

--- pre_install.sh
+++ pre_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-1-amd64.deb
-dpkg -i pandoc-3.1.5-1-arm64.deb --force-not-root --root=/usr/local
+wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-linux-amd64.tar.gz
+tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C /usr/local
+export PATH="/usr/local:$PATH"
