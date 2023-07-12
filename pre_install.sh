1934931c6cf91a41ffbbcf5e376409585092abd9
Patrick Nicodemus <gadget142@gmail.com>
2023-07-12 14:21:51 -0400

    g

--- pre_install.sh
+++ pre_install.sh
@@ -2,6 +2,6 @@
 
 wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-linux-amd64.tar.gz
 tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C ~/.local
-export PATH="~/.local/bin:$PATH"
+export PATH="~/.local/bin:pandoc-3.1.5/bin:$PATH"
 echo $(whereis pandoc)
 echo $(which pandoc)
