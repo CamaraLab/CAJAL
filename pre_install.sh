aa29b5a31f23af949653da9ff4a036d9b1838c67
Patrick Nicodemus <gadget142@gmail.com>
2023-07-12 14:33:19 -0400

    trying again

--- pre_install.sh
+++ pre_install.sh
@@ -2,9 +2,8 @@
 
 wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-linux-amd64.tar.gz
 mkdir pandoc-dir
-tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C $HOME/.local
-tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C pandoc-dir
-export PATH="~/.local/bin:pandoc-3.1.5/bin:pandoc-dir/pandoc-3.1.5/bin:$PATH"
+tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C /home/docs/pandoc
+export PATH="/home/docs/pandoc/bin:$PATH"
 echo $PATH
 echo $(whereis pandoc)
 echo $(which pandoc)
