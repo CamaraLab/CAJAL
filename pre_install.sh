b6d3683a9ac9fb911213295b92f4468dfec93279
Patrick Nicodemus <gadget142@gmail.com>
2023-07-12 14:17:57 -0400

    Fixed dumb error.

--- pre_install.sh
+++ pre_install.sh
@@ -2,6 +2,6 @@
 
 wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-linux-amd64.tar.gz
 tar -xvzf pandoc-3.1.5-linux-amd64.tar.gz --strip-components 1 -C ~/.local
-export PATH="~/.local:$PATH"
+export PATH="~/.local/bin:$PATH"
 echo $(whereis pandoc)
 echo $(which pandoc)
