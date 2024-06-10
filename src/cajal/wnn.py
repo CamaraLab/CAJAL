8f58ffa02d9782f1b8a282e1fb2eaf2b10a20026
Patrick Nicodemus <gadget142@gmail.com>
2024-06-10 12:40:39 -0400

    First draft of wnn w multiple modalities.

--- src/cajal/wnn.py
+++ src/cajal/wnn.py
@@ -1,33 +1,146 @@
+"""
+n-modality version of the weighted nearest neighbors algorithm
+"""
+
+from typing import Optional
+import itertools as it
+
 import numpy as np
 import numpy.typing as npt
 from sklearn.manifold import Isomap
 from skdim.id import MADA
-import anndata as ad
-
+from scipy.spatial.distance import squareform, pdist
+from scipy.special import softmax
 from .run_gw import DistanceMatrix
-from .pyWNN import *
 
 
-def wnn(gw_dmat: DistanceMatrix,
-        features: npt.NDArray[np.float_],
-        # feature_dmat
-        n_neighbors=20,
-        # gw_dimension=None
-        ):
+class Modality:
+    def local_bandwidth(self, margin_count: int = 20):
+        n_obsv = self.nn_index_arr.shape[0]
+        jaccard_counts = np.zeros(shape=self.nn_index_arr.shape, dtype=int)
+        for i, j in it.combinations(range(0, n_obsv),2):
+            jaccard_counts[i, j] = np.intersect1d(
+                self.nn_index_arr[i], self.nn_index_arr[j]
+            ).shape[0]
+        jaccard_counts = np.maximum(jaccard_counts, jaccard_counts.T)
+        local_bandwidths = []
+        for i in range(n_obsv):
+            jaccard_argsort = np.argsort(jaccard_counts[i])
+            # jaccard_argsort_filtered
+            jaccard_argsort_filtered = jaccard_argsort[
+                np.nonzero(np.sort(jaccard_counts[i]))[0]
+            ]
+            if jaccard_argsort_filtered.shape[0] > margin_count:
+                jaccard_argsort_filtered = jaccard_argsort_filtered[:margin_count]
+            marginal_obsvns = self.obsv[jaccard_argsort_filtered]
+            avg_distance = (
+                np.linalg.norm(self.obsv[i] - marginal_obsvns, axis=1).sum()
+                / jaccard_argsort_filtered.shape[0]
+            )
+            local_bandwidths.append(avg_distance)
+        return np.array(local_bandwidths)
+
+    def __init__(self, obsv: npt.NDArray[np.float_]):
+        """
+        The primary constructor for the Modality class is used when the user has direct access to
+        a sequence of observations in n dimensional space.
+
+        :param obsv: A `k` row by `n` column matrix, where `k` is the number of observations,
+            and `n` is the dimensionality of the space from which the observations are taken.
+        """
+        self.obsv = obsv
+        self.dmat: DistanceMatrix = squareform(pdist(obsv))
+        self.nn_index_arr = np.argsort(self.dmat, axis=1)
+        self.bandwidth = self.local_bandwidth()
+        self.nn_distance = self.dmat[:, self.nn_index_arr[:, 1]]
+
+    @staticmethod
+    def of_dmat(dmat: DistanceMatrix, intrinsic_dim: Optional[int] = None, n_neighbors=20):
+        m = Modality(np.array([[0,1],[1,0]]))
+        if intrinsic_dim is None:
+            intrinsic_dim: int = int(MADA(DM=True).fit_transform(np.copy(dmat)))
+        embedding = Isomap(
+            n_neighbors=n_neighbors, n_components=intrinsic_dim, metric="precomputed"
+        ).fit_transform(np.copy(dmat))
+        m.obsv = embedding
+        m.dmat = dmat
+        m.nn_index_arr = np.argsort(dmat, axis=1)
+        m.bandwidth = m.local_bandwidth()
+        m.nn_distance = m.dmat[:, m.nn_index_arr[:, 1]]
+        return m
+
+
+def wnn(modalities: list[Modality], k: int, epsilon: float = 1e-4):
     """
-    :param gw_dmat: A Gromov-Wasserstein distance matrix, `n x n`.
-    :param features: An `n x m` matrix, where each row is a feature vector for
-        one of the cells.
-    """
+    Compute the weighted nearest neighbors pairing, following
+    [Integrated analysis of multimodal single-cell data]\
+    (https://www.sciencedirect.com/science/article/pii/S0092867421005833.)
 
-    # Embed the Gromov-Wasserstein distance matrix into Euclidean
-    # space via the Isomap algorithm.
-    intrinsic_dim: int = int(MADA(DM=True).fit_transform(np.copy(gw_dmat)))
-    gw_embedding=Isomap(n_neighbors=n_neighbors, n_components=intrinsic_dim,
-                        metric='precomputed').fit_transform(np.copy(gw_dmat))
-    adata = ad.AnnData(shape=gw_embedding.shape)
-    adata.obsm['gw'] = gw_embedding
-    adata.obsm['features'] = features
-    adata = pyWNN(adata, reps=['gw', 'features'], n_neighbors = n_neighbors, npcs=[intrinsic_dim, features.shape[1]]).compute_wnn(adata)
-    return adata
-    # adata = WNNobj.compute_wnn(adata)
+    This algorithm differs from the published algorithm in the paper in a few ways.
+    In particular we do not take the L2 normalization of columns of the matrix before we begin.
+
+    :param modalities: list of modalities
+    :param k: how many nearest neighbors to consider
+    """
+    if k <= 0:
+        raise Exception("k should be a positive integer.")
+    if ~modalities:
+        raise Exception("Empty list.")
+    n_obsvs: int = modalities[0].obsv.shape[0]
+    for modality in modalities[1:]:
+        if modality.obsv.shape[0] != n_obsvs:
+            raise Exception(
+                "Must be a consistent count of exceptions across all modalities."
+            )
+
+    # Notation follows the paper when appropriate.
+    #
+    M = len(modalities)
+
+    def pairwise_predictions(m: Modality, n: Modality):
+        """
+        We measure the ability of modality m to predict modality n.
+        """
+        prediction_vectors = n.obsv[m.nn_index_arr[:, 1 : (k + 1)], :].sum(axis=1) / k
+        return prediction_vectors
+
+    def cross_modality_affinities(
+        m: Modality, prediction_vectors: npt.NDArray[np.float_]
+    ):
+        """
+        :param m: "target" modality (to be predicted)
+        :param prediction_vectors: `n_obsvs` rows, `k_n` columns,
+            where `k_n` is the dimension of Euclidean space for the target modality `m`.
+            The vectors for feature `m` predicted using the nearest neighbors in `n`.
+        """
+        prediction_distance = np.linalg.norm(prediction_vectors - m.obsv, axis=1)
+        return np.exp(
+            -np.maximum(prediction_distance - m.nn_distance, 0)
+            / (m.local_bandwidth - m.nn_distance)
+        )
+
+    def all_pairwise_affinities(modalities: list[modalities]):
+        pairwise_affinities = np.zeros(shape=(M, M, n_obsvs), dtype=float)
+        for i in range(M):
+            m = modalities[i]
+            for j in range(M):
+                n = modalities[j]
+                # theta_m,n = pairwise_affinities[i,j]
+                # ability of j to predict i, ability of n to predict m
+                pairwise_affinities[i, j] = cross_modality_affinities(
+                    m, pairwise_predictions(n, m)
+                )
+
+        pairwise_affinity_ratios = np.zeros(shape=(M, M - 1, n_obsvs), dtype=float)
+        for i in range(M):
+            for j in range(M):
+                if j >= i:
+                    k = j - 1
+                else:
+                    k = j
+                pairwise_affinity_ratios[i, k, :] = pairwise_affinities[i, i, :] / (
+                    pairwise_affinities[i, k, :] + epsilon
+                )
+        return softmax(pairwise_affinity_ratios, axis=1)
+
+    return all_pairwise_affinities(modalities)
