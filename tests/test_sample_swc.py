545e128a7129a0ee8ed8a6738ef9639281a472b2
Patrick Nicodemus <gadget142@gmail.com>
2024-06-17 14:41:58 -0400

    Updated tests.

--- tests/test_sample_swc.py
+++ tests/test_sample_swc.py
@@ -41,7 +41,6 @@ def test_compute_icdm_both():
         infolder=swc_dir,
         out_csv="tests/icdm_euclidean.csv",
         n_sample=50,
-        num_processes=10,
     )
     compute_icdm_all_geodesic(
         infolder=swc_dir,
