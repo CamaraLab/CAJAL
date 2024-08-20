fcbcad3b6f77cdad99df1c17f970ead7995aff2b
Patrick Nicodemus <patrick.nicodemus@pennmedicine.upenn.edu>
2024-08-20 12:10:48 -0400

    Fixed bug in Cython build.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ ROOT = os.path.abspath(os.path.dirname(__file__))
 include_path = [numpy.get_include()]
 
 setup(
-    ext_modules=cythonize(["src/cajal/*.pyx", "src/cajal/EMD_wrapper.cpp"]),
+    ext_modules=cythonize(["src/cajal/*.pyx"]),
     compiler_directives={"language_level": "3"},
     include_dirs=include_path,
 )
--- src/cajal/gw_cython.pxd
+++
@@ -1,6 +0,0 @@
-# distutils: language=c++
-
-from libc.stdint cimport uint64_t
-cdef extern from "EMD.h":
-     cdef int myCMessage(int a, int b)
-     cdef int EMD_wrap(int n1, int n2, double *X, double *Y, double *D, double *G, double* alpha, double* beta, double *cost, uint64_t maxIter) nogil
