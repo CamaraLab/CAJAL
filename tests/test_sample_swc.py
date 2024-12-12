a531f0ed3bbc062017b931868cc0d211c325af93
Patrick Nicodemus <patrick.nicodemus@pennmedicine.upenn.edu>
2024-12-12 11:49:28 -0500

    Formtting change.

--- tests/test_sample_swc.py
+++ tests/test_sample_swc.py
@@ -5,7 +5,7 @@ from src.cajal.sample_swc import (
     read_preprocess_compute_euclidean,
     compute_icdm_all_euclidean,
     compute_icdm_all_geodesic,
-    fused_gromov_wasserstein_parallel
+    fused_gromov_wasserstein_parallel,
 )
 from src.cajal.utilities import Err
 import os
@@ -43,7 +43,7 @@ def test_fgw_par():
     compute_icdm_all_euclidean(
         infolder=swc_dir,
         out_csv=out_eu_csv,
-        out_node_types= out_eu_nt,
+        out_node_types=out_eu_nt,
         n_sample=50,
         num_processes=10,
     )
@@ -58,14 +58,14 @@ def test_fgw_par():
     out_geo_nt = "tests/geo_node_types.npy"
 
     fused_gromov_wasserstein_parallel(
-        intracell_csv_loc = out_geo_csv,
-        swc_node_types = out_geo_nt,
-        fgw_dist_csv_loc = "tests/geo_fgw.csv",
-        num_processes = 2,
-        soma_dendrite_penalty = 1000.,
-        basal_apical_penalty = 1000.,
+        intracell_csv_loc=out_geo_csv,
+        swc_node_types=out_geo_nt,
+        fgw_dist_csv_loc="tests/geo_fgw.csv",
+        num_processes=2,
+        soma_dendrite_penalty=1000.0,
+        basal_apical_penalty=1000.0,
     )
-    
+
     os.remove(out_eu_csv)
     os.remove(out_geo_csv)
     os.remove(out_eu_nt)
