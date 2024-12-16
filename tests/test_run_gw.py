d9b7ea8a34f10529731b4793f8c6ef6daa043e58
Patrick Nicodemus <patrick.nicodemus@pennmedicine.upenn.edu>
2024-12-16 11:20:19 -0500

    Tests pass

--- tests/test_run_gw.py
+++ tests/test_run_gw.py
@@ -1,4 +1,5 @@
-from cajal.run_gw import compute_gw_distance_matrix
+from src.cajal.run_gw import compute_gw_distance_matrix
+from src.cajal.utilities import read_gw_dists, read_gw_couplings, cell_iterator_csv, avg_shape_spt
 import os
 
 
@@ -7,9 +8,8 @@ def test():
         intracell_csv_loc="tests/icdm.csv",
         gw_dist_csv_loc="tests/gw1.csv",
         num_processes=2,
-        gw_coupling_mat_csv_loc=None,
+        gw_coupling_mat_npz_loc=None,
         return_coupling_mats=False,
-        verbose=False,
     )
     os.remove("tests/gw1.csv")
     compute_gw_distance_matrix(
@@ -17,6 +17,12 @@ def test():
         gw_dist_csv_loc="tests/gw.csv",
         num_processes=2,
         return_coupling_mats=True,
-        gw_coupling_mat_csv_loc="tests/gw_coupling_mat.csv",
-        verbose=False,
+        gw_coupling_mat_npz_loc="tests/gw_coupling_mat.npz",
     )
+
+    names, gw_dists = read_gw_dists("tests/gw.csv", header=True)
+    gw_coupling_mat_dict = read_gw_couplings("tests/gw_coupling_mat.npz")
+    cell_icdms = dict(cell_iterator_csv("tests/icdm.csv", as_squareform=False))
+    avg_shape_spt(names, gw_dists, cell_icdms,
+                  gw_coupling_mat_dict=gw_coupling_mat_dict,
+                  k=7)
--- tests/test_sample_swc.py
+++ tests/test_sample_swc.py
@@ -5,7 +5,6 @@ from src.cajal.sample_swc import (
     read_preprocess_compute_euclidean,
     compute_icdm_all_euclidean,
     compute_icdm_all_geodesic,
-    fused_gromov_wasserstein_parallel,
 )
 from src.cajal.utilities import Err
 import os
@@ -34,39 +33,3 @@ def test_rpcg():
     for i, a in enumerate(t):
         if i > 20:
             break
-
-
-def test_fgw_par():
-    swc_dir = "tests/swc"
-    out_eu_csv = "tests/icdm_euclidean.csv"
-    out_eu_nt = "tests/eu_node_types.npy"
-    compute_icdm_all_euclidean(
-        infolder=swc_dir,
-        out_csv=out_eu_csv,
-        out_node_types=out_eu_nt,
-        n_sample=50,
-        num_processes=10,
-    )
-    compute_icdm_all_geodesic(
-        infolder=swc_dir,
-        out_csv="tests/icdm_geodesic.csv",
-        out_node_types="tests/geo_node_types.npy",
-        n_sample=50,
-        num_processes=10,
-    )
-    out_geo_csv = "tests/icdm_geodesic.csv"
-    out_geo_nt = "tests/geo_node_types.npy"
-
-    fused_gromov_wasserstein_parallel(
-        intracell_csv_loc=out_geo_csv,
-        swc_node_types=out_geo_nt,
-        fgw_dist_csv_loc="tests/geo_fgw.csv",
-        num_processes=2,
-        soma_dendrite_penalty=1000.0,
-        basal_apical_penalty=1000.0,
-    )
-
-    os.remove(out_eu_csv)
-    os.remove(out_geo_csv)
-    os.remove(out_eu_nt)
-    os.remove(out_geo_nt)
