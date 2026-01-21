e3ece15cdd66ed3369d4c148df7868087ee2c024
Lincoln Wu <73685301+lincolnwu@users.noreply.github.com>
2026-01-21 10:51:26 -0500

    Update test_run_gw.py

--- tests/test_run_gw.py
+++ tests/test_run_gw.py
@@ -1,5 +1,4 @@
-# from cajal.run_gw import compute_gw_distance_matrix, compute_gw_distance_matrix_query_target
-from src.cajal.run_gw import compute_gw_distance_matrix, compute_gw_distance_matrix_query_target
+from src.cajal.run_gw import compute_gw_distance_matrix
 from src.cajal.utilities import (
     read_gw_dists,
     read_gw_couplings,
@@ -32,33 +31,3 @@ def test():
     avg_shape_spt(
         names, gw_dists, cell_icdms, gw_coupling_mat_dict=gw_coupling_mat_dict, k=7
     )
-
-
-def test_query_target():
-    intracell_csv_loc="tests/icdm.csv"
-
-    # Test run with saving data
-    dists, _ = compute_gw_distance_matrix_query_target(
-        query_intracell_csv_loc=intracell_csv_loc,
-        target_intracell_csv_loc=intracell_csv_loc,
-        gw_dist_csv_loc="tests/gw.csv",
-        num_processes=2,
-        return_coupling_mats=True,
-        gw_coupling_mat_csv_loc="tests/gw_coupling_mat.csv",
-        verbose=False,
-    )
-    os.remove("tests/gw.csv")
-    os.remove("tests/gw_coupling_mat.csv")
-
-    # Make sure results are consistent with all-by-all
-    dists2, _ = compute_gw_distance_matrix(
-        intracell_csv_loc=intracell_csv_loc,
-        gw_dist_csv_loc=None,
-        num_processes=2,
-        return_coupling_mats=True,
-        gw_coupling_mat_csv_loc=None,
-        verbose=False,
-    )
-
-    assert (dists.round(5) == dists2.round(5)).all()
-
