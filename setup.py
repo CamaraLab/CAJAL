630a49edd0ff62300b89a78e2acd8b68e0210cc8
Robert Hu <robkylehu@gmail.com>
2026-06-15 12:00:31 -0400

    Added MANIFEST to include missing source files during build. Added option to avoid ugw installation for testing purposes.

--- setup.py
+++ setup.py
@@ -12,17 +12,30 @@ class build(_build):
         if (platform == "win32"):
             self.compiler = "mingw32"
 
+
+def skip_ugw_build() -> bool:
+    value = os.environ.get("CAJAL_SKIP_UGW", "")
+    return value.lower() in {"1", "true", "yes", "on"}
+
 ROOT = os.path.abspath(os.path.dirname(__file__))
 include_path = [numpy.get_include()]
 
+ext_modules = cythonize(["src/cajal/*.pyx"])
+
+if skip_ugw_build():
+    cffi_modules = []
+else:
+    cffi_modules = [
+        "src/cajal/ugw/build_backends.py:build_single_core",
+        "src/cajal/ugw/build_backends.py:build_multicore",
+        # "src/cajal/ugw/build_backends.py:build_opencl",
+        # "src/cajal/ugw/build_backends.py:build_cuda",
+    ]
+
 setup(
-    ext_modules=cythonize(["src/cajal/*.pyx"]),
+    ext_modules=ext_modules,
     compiler_directives={"language_level": "3"},
     include_dirs=include_path,
-    cffi_modules=["src/cajal/ugw/build_backends.py:build_single_core",
-                  "src/cajal/ugw/build_backends.py:build_multicore",
-                  # "src/cajal/ugw/build_backends.py:build_opencl",
-                  # "src/cajal/ugw/build_backends.py:build_cuda",
-                  ],
+    cffi_modules=cffi_modules,
     cmdclass={"build": build},
 )
